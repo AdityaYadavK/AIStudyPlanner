0ce7a5616373787109ca30e2a478ef98a9ada903
AdityaYadavK <AdityaYadavK@proton.me>
2026-08-23 12:18:54 +0530

    project description and synopsis

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/dashboard.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
 
   @override
